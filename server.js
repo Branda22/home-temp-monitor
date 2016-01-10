fbbccfb7756bdb34072d2a58ee5cd1c101011518
Christian Brandalise <christian.brandalise@gmail.com>
2016-01-10 14:09:57 -0600

    bodyparser settings

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = Express();
 app.set('port', (process.env.PORT || 5000));
 app.use(Express.static('public'));
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));

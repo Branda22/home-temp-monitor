7abb158d6c1e52b4c5a44d0afcf5b14f7771b4b5
Christian Brandalise <christian.brandalise@gmail.com>
2016-01-09 18:33:35 -0600

    signup and login implemented with bcrypt, error handling for improper login

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@
  */
 
 var db = require('../db/db.js');
+var bcrypt = require('bcrypt');
 
 var Routes = function(app) {
     app.get('/', function(req, res) {
@@ -14,11 +15,41 @@ var Routes = function(app) {
     });
 
     app.post('/signup', function(req, res) {
+        var userInfo = req.body;
 
+        if (Object.keys(req.body).length > 0) {
+            bcrypt.hash(userInfo.password, 8, function(err, hash) {
+                if(err) return console.error('hashing error', err);
+                var client = db.getDBConnection();
+                client.query("INSERT INTO users(email, user_name, password) values($1, $2, $3)",
+                    [userInfo.email, userInfo.user, hash], function(err, result) {
+                        if(err) return console.error('Error inserting the user', err);
+                        console.log('RECORD INSERTED INTO users table');
+                        res.render('dashboard');
+                    });
+            });
+        }
     });
 
-    app.post('/login', function(req, res) {
-
+    app.post('/login', function(req, response) {
+        var loginInfo = req.body;
+        var client = db.getDBConnection();
+        client.query('SELECT * FROM users WHERE user_name = $1',
+            [loginInfo.user], function(err, res) {
+            if(err) return console.error('SELECT error', err);
+            if(res.rows.length) {
+                bcrypt.compare(loginInfo.password, res.rows[0].password, function(err, res){
+                    if(err) return console.error('bcrypt error', err);
+                    if(res) response.render('dashboard');
+                });
+            } else {
+                response.render('index', {
+                    error: {
+                        message: 'User ' + loginInfo.user +  ' not found'
+                    }
+                });
+            }
+        });
     });
 };
 

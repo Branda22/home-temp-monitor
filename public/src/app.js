7b7b7dc3390cf2fef77710a7318f31933e8ec418
Christian Brandalise <christian.brandalise@gmail.com>
2016-01-10 21:40:11 -0600

    turns off chart appending

--- public/src/app.js
+++ public/src/app.js
@@ -2,10 +2,10 @@ $(function(window, $){
     var socket = io();
     socket.on('newTemp', function(data) {
         console.log(data);
-        var date = new Date();
+        var date = Date.now();
         var $2ndFloor = $('.second-floor-temp');
         $2ndFloor.find('p').text(data.temperature + "C");
         $2ndFloor.find('p').next().text('Last reading on: ' + date);
-        $('.chart-container').highcharts().series[0].addPoint([date, parseFloat(data.temperature)]);
+        //$('.chart-container').highcharts().series[0].addPoint([date, parseFloat(data.temperature)]);
     });
 }(window, $));

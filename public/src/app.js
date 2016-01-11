9a0d44125c77fc86ab426865b9e063035d26c50e
Christian Brandalise <christian.brandalise@gmail.com>
2016-01-10 21:26:08 -0600

    adds arduino temp realtime

--- public/src/app.js
+++ public/src/app.js
@@ -2,5 +2,9 @@ $(function(window, $){
     var socket = io();
     socket.on('newTemp', function(data) {
         console.log(data);
+        var date = new Date();
+        var $2ndFloor = $('.second-floor-temp');
+        $2ndFloor.find('p').text(data.temperature + "C");
+        $2ndFloor.find('p').next().text('Last reading on: ' + date);
     });
 }(window, $));

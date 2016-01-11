dec4d0167c48d3c3ceceaebadd1dc57a1459880c
Christian Brandalise <christian.brandalise@gmail.com>
2016-01-10 21:44:42 -0600

    date fix

--- public/src/app.js
+++ public/src/app.js
@@ -2,7 +2,7 @@ $(function(window, $){
     var socket = io();
     socket.on('newTemp', function(data) {
         console.log(data);
-        var date = Date.now();
+        var date = new Date();
         var $2ndFloor = $('.second-floor-temp');
         $2ndFloor.find('p').text(data.temperature + "C");
         $2ndFloor.find('p').next().text('Last reading on: ' + date);

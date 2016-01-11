6e3acf3d0f9fa1225b1f5af27421c06a914ebeca
Christian Brandalise <christian.brandalise@gmail.com>
2016-01-10 21:34:29 -0600

    appends data to chart in realtime

--- public/src/app.js
+++ public/src/app.js
@@ -6,5 +6,6 @@ $(function(window, $){
         var $2ndFloor = $('.second-floor-temp');
         $2ndFloor.find('p').text(data.temperature + "C");
         $2ndFloor.find('p').next().text('Last reading on: ' + date);
+        $('.chart-container').highcharts().series[0].addPoint([date, parseFloat(data.temperature)]);
     });
 }(window, $));
